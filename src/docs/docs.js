990731271e2ab2f458f7a2c40c66bc365e99203b
lucastobrazil <lucas.arundell@gmail.com>
2018-10-25 00:25:33 +0200

    feat(Docs) change heading

--- src/docs/docs.js
+++ src/docs/docs.js
@@ -1,17 +1,12 @@
-import { HeadingOne } from 'cabana-react';
+import { HeadingThree } from 'cabana-react';
 import React from 'react';
 export default function Docs({ title, children, style }) {
     return (
         <div style={style}>
             <a name={title.split(' ').join('')} />
-            <HeadingOne
-                fontWeight="bold"
-                mt={8}
-                mb={2}
-                style={{ display: 'block', borderBottom: '1px solid lightgrey' }}
-            >
+            <HeadingThree is="h1" mt={8} mb={2} style={{ display: 'block', borderBottom: '1px solid lightgrey' }}>
                 {title}
-            </HeadingOne>
+            </HeadingThree>
             {children}
         </div>
     );
