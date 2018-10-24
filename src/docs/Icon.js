20653a34877493e8cc7e144ab650af80e54283e1
lucastobrazil <lucas.arundell@gmail.com>
2018-10-25 00:23:59 +0200

    feat(Icon) create docs

--- src/docs/Icon.js
+++ src/docs/Icon.js
@@ -1,10 +1,13 @@
 import Docs from './docs';
-import { Icon } from 'cabana-react';
+import { BodyText } from 'cabana-react';
+import { IconHeart, IconTwitter } from 'cabanaico';
 import React from 'react';
 export default function IconDocs() {
     return (
         <Docs title="Icons">
-            <Icon />
+            <BodyText>We use the cabanaico library</BodyText>
+            <IconHeart />
+            <IconTwitter />
         </Docs>
     );
 }
