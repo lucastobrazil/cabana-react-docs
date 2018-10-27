af8dbc0cc814a1b05d32e4892a1d567b411a0a49
lucastobrazil <lucas.arundell@gmail.com>
2018-10-27 23:04:57 +0200

    feat(Docs) syntax highlighting ✨

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Box, Tabs, HeroText, LeadText, BodyText, TextPrimitive, CaptionText } from 'cabana-react';
+import SyntaxHighLighter from 'react-syntax-highlighter/prism';
+import { tomorrow } from 'react-syntax-highlighter/styles/prism';
 
 const Section = props => <Box maxWidth={900} py={4} mx="auto" {...props} />;
+const ListItem = props => <TextPrimitive is="li" {...props} />;
 export default function Home() {
     return (
         <Box>
             <Section>
-                <HeroText letterSpacing="-0.05em" color="darkGrey" textAlign="center" fontWeight="bold">
+                <HeroText letterSpacing="-0.05em" color="coyGrey" textAlign="center" fontWeight="bold">
                     Design & Build Better, Smarter, and Faster in Sketch and React.
                 </HeroText>
                 <BodyText textAlign="center">
@@ -23,18 +26,31 @@ export default function Home() {
                 </Section>
             </Box>
             <Section display="flex">
-                <Box flexBasis="50%">
+                <Box>
+                    <LeadText>For Designers</LeadText>
+                    <ul>
+                        <ListItem>Sketch Library with powerful customisation</ListItem>
+                        <ListItem>
+                            Flexible visual style, whilst also keeping design system ‘rules’ under control
+                        </ListItem>
+                    </ul>
+                    <CaptionText display="block" fontWeight="bold">
+                        Sketch Layer Styles
+                    </CaptionText>
+                    <img style={{ maxWidth: 300 }} src={require('../images/colors_sketch.png')} />
+                </Box>
+                <Box flex="0 0 50%">
                     <LeadText>For Developers</LeadText>
                     <ul>
-                        <li>React</li>
-                        <li>Works out of the box</li>
-                        <li>Flexible and Composable</li>
-                        <li>Separation of Concerns</li>
+                        <ListItem>React</ListItem>
+                        <ListItem>Works out of the box</ListItem>
+                        <ListItem>Flexible and Composable</ListItem>
+                        <ListItem>Separation of Concerns</ListItem>
                     </ul>
                     <CaptionText display="block" fontWeight="bold">
                         Theme JS object
                     </CaptionText>
-                    <pre>
+                    <SyntaxHighLighter customStyle={{ fontSize: '12px' }} style={tomorrow} language="javascript">
                         {`
 const theme = {
     colors: {
@@ -43,29 +59,14 @@ const theme = {
         tertiary: '#EF476F',
         black: '#303133',
         grey: '#8D8D8F',
-        lightGrey: '#DADADA',
-        darkGrey: '#303133',
+        LightGrey: '#DADADA',
         success: '#44A45F',
         warning: '#FCEB3B',
         error: '#E6173E',
-        white: '#ffffff',
-        inputFocus: '#4688F1',
-        inputDisabledBg: 'rgba(196, 196, 196, .3)',
     }   
-}
+};
                         `}
-                    </pre>
-                </Box>
-                <Box>
-                    <LeadText>For Designers</LeadText>
-                    <ul>
-                        <li>Sketch Library with powerful customisation</li>
-                        <li>Flexible visual style, whilst also keeping design system ‘rules’ under control</li>
-                    </ul>
-                    <CaptionText display="block" fontWeight="bold">
-                        Sketch Layer Styles
-                    </CaptionText>
-                    <img style={{ maxWidth: 300 }} src={require('../images/colors_sketch.png')} />
+                    </SyntaxHighLighter>
                 </Box>
             </Section>
             <Box bg="tertiary">
@@ -85,15 +86,15 @@ const theme = {
                 </BodyText>
                 <BodyText>
                     As your system grows, you'll notice that your language changes from 'box', 'text' etc and into more
-                    metaphoric terms like 'Card' and 'Hero Heading'. Cabana is a system that connects this language
+                    metaphoric terms Like 'Card' and 'Hero Heading'. Cabana is a system that connects this language
                     across design (Sketch) and development (React).
                 </BodyText>
                 <ul>
-                    <li>Speaking the same language (constraints-based)</li>
-                    <li>Rapid prototyping </li>
-                    <li>Rapid prototyping </li>
-                    <li>Theme-based design system </li>
-                    <li>DRY from the beginning</li>
+                    <ListItem>Speaking the same language (constraints-based)</ListItem>
+                    <ListItem>Rapid prototyping </ListItem>
+                    <ListItem>Rapid prototyping </ListItem>
+                    <ListItem>Theme-based design system </ListItem>
+                    <ListItem>DRY from the beginning</ListItem>
                 </ul>
                 <Tabs>
                     <Box fontWeight="normal" fontSize="body" label="Sketch">
