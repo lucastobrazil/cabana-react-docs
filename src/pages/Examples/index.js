16a5fff35bb47e7e0f9c3ef5a3575da4cfa8091a
lucastobrazil <lucas.arundell@gmail.com>
2018-12-14 09:45:50 +1000

    feat(Examples) add basic mobile nav

--- src/pages/Examples/index.js
+++ src/pages/Examples/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Box, Button, Card, CardBodyText, CardImage, LeadText, HeadingThree, HeadingFive } from 'cabana-react';
+import { IconHome, IconCart, IconSettings, IconPodcast, IconEq, IconMenu, IconEuro } from 'cabanaico';
 
 export default function Examples() {
   return (
@@ -28,26 +29,29 @@ const items = [
 
 const Item = ({ name, description, price }) => (
   <Box flex="0 0 110%" mx={3}>
-    <Card textAlign="left">
+    <Card textAlign="left" borderColor="transparent">
       <CardImage src={require('../../images/audio_tech.png')} />
       <HeadingFive fontWeight="bold">{name}</HeadingFive>
       <CardBodyText>{description}</CardBodyText>
     </Card>
-    <Box display="flex" my={3}>
+    <Box display="flex" my={3} alignItems="center">
       <LeadText color="white" fontWeight="bold">{`$${price} + VAT`}</LeadText>
-      <Button bg="primary" color="white">
-        Buy Now
+      <Button bg="primary" color="white" ml={4}>
+        Buy Now <IconEuro />
       </Button>
     </Box>
   </Box>
 );
 function PhoneExampleOne() {
   return (
-    <Box position="relative" maxWidth={375} borderColor="primary" borders="0px solid" borderTop={333} bg="darkGrey">
-      <Box mt={-333} mb={72}>
-        <HeadingThree fontWeight="bold" color="white" mb={3} p={3}>
-          Discover
-        </HeadingThree>
+    <Box position="relative" maxWidth={375} borderColor="primary" borders="0px solid" borderTop={333} bg="darkGrey" pb={72}>
+      <Box mt={-333}>
+        <Box display="flex" color="white" alignItems="center" justifyContent="space-between" p={3}>
+          <HeadingThree fontWeight="bold" >
+            Discover
+          </HeadingThree>
+          <IconMenu />
+        </Box>
         <Box display="flex" style={{ overflowY: 'scroll' }} width="100%">
           {items.map(item => (
             <Item key={item.id} {...item} />
@@ -55,8 +59,28 @@ function PhoneExampleOne() {
         </Box>
       </Box>
 
-      <Box position="absolute" bottom={0} left={0} bg="primary" width="100%" height={72}>
-        FOOTER
+      <Box
+        position="absolute"
+        display="flex"
+        alignItems="center"
+        justifyContent="space-between"
+        px={2}
+        maxWidth="100%"
+        bottom={0}
+        left={0}
+        bg="primary"
+        width="100%"
+        height={72}
+        color="lightGrey"
+        style={{ boxSizing: 'border-box' }}
+      >
+        <Box color="white">
+          <IconHome />
+        </Box>
+        <IconCart />
+        <IconSettings />
+        <IconPodcast />
+        <IconEq />
       </Box>
     </Box>
   );
