60479db3a26a7af1f0916444ff63e9c5f42a2b25
lucastobrazil <lucas.arundell@gmail.com>
2019-01-12 22:32:30 +0100

    Remove duplicate prod flag

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,13 +1,4 @@
 const config = require('./webpack.config.js');
-const webpack = require('webpack');
-
-config.plugins.push(
-  new webpack.DefinePlugin({
-    'process.env': {
-      NODE_ENV: JSON.stringify('production'),
-    },
-  })
-);
 
 config.optimization.minimize = true;
 
